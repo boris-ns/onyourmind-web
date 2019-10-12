0f17c43378ec976892a6fa9bbc7d85a79ebe0f4f
Boris Sulicenko <b.sulicenko@gmail.com>
2019-10-12 21:57:00 +0200

    Added tooltips to some places

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,17 +1,17 @@
 import { MatButtonModule, MatCheckboxModule, MatInputModule, 
     MatIconModule, MatToolbarModule, MatMenuModule, MatTableModule,
-    MatSlideToggleModule, MatCardModule
+    MatSlideToggleModule, MatCardModule, MatTooltipModule
 } from '@angular/material';
 import { NgModule } from '@angular/core';
 
 @NgModule({
     imports: [MatButtonModule, MatCheckboxModule, MatInputModule, 
         MatIconModule, MatToolbarModule, MatMenuModule, MatTableModule,
-        MatSlideToggleModule, MatCardModule
+        MatSlideToggleModule, MatCardModule, MatTooltipModule
     ],
     exports: [MatButtonModule, MatCheckboxModule, MatInputModule, 
         MatIconModule, MatToolbarModule, MatMenuModule, MatTableModule,
-        MatSlideToggleModule, MatCardModule
+        MatSlideToggleModule, MatCardModule, MatTooltipModule
     ]
 })
 export class MaterialModule {
